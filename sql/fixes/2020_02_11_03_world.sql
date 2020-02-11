0d3b38aeeb46c4b4a319f0b9ba07779a20e2c756
Rushor <PBienati@web.de>
2020-02-11 21:14:53 +0100

    DB/Gameobject: Dalaran- Add missing visual forge to "Forge of Fate".

---
+++ sql/fixes/2020_02_11_03_world.sql
@@ -0,0 +1,8 @@
+-- 
+DELETE FROM `gameobject` WHERE `guid` = 2134391;
+INSERT INTO `gameobject` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`, `ScriptName`, `VerifiedBuild`) VALUES 
+(2134391, 411521, 571, 4395, 4395, 1, 1, 5919.97, 692.543, 642.482, 2.67735, 0, 0, -0.333807, 0.942641, 300, 100, 1, '', 0);
+
+DELETE FROM `gameobject_template` WHERE `entry` = 411521;
+INSERT INTO `gameobject_template` (`entry`, `type`, `displayId`, `name`, `IconName`, `castBarCaption`, `unk1`, `size`, `Data0`, `Data1`, `Data2`, `Data3`, `Data4`, `Data5`, `Data6`, `Data7`, `Data8`, `Data9`, `Data10`, `Data11`, `Data12`, `Data13`, `Data14`, `Data15`, `Data16`, `Data17`, `Data18`, `Data19`, `Data20`, `Data21`, `Data22`, `Data23`, `AIName`, `ScriptName`, `VerifiedBuild`) VALUES 
+(411521, 8, 305, 'Small Forge', '', '', '', 0.01, 3, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0);
