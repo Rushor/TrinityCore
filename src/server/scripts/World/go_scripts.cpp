5cf26341d242e9c6e36068970de5585a2f690050
Rushor <PBienati@web.de>
2020-02-21 21:02:13 +0100

    Scripts/Gameobject: Implement questevent for Chambermaid Pillaclencher.

--- src/server/scripts/World/go_scripts.cpp
+++ src/server/scripts/World/go_scripts.cpp
@@ -1973,6 +1973,115 @@ public:
     }
 };
 
+enum PillowFight
+{
+    NPC_SLEEPY_DARK_IRON_WORKER                 = 14635,
+    NPC_CHAMBERMAID_PILLACLENCHER               = 14636,
+    GAMEOBJECT_DARK_IRON_PILLOW_EVENT_GENERATOR = 179829,
+
+    EVENT_WAVE_1                                = 1,
+    EVENT_WAVE_2                                = 2,
+    EVENT_CHAMBERMAID                           = 3,
+
+    SAY_IRON_WORKER_0                           = 0,
+    SAY_IRON_WORKER_1                           = 1
+};
+
+class go_dark_iron_pillow : public GameObjectScript
+{
+    public:
+        go_dark_iron_pillow() : GameObjectScript("go_dark_iron_pillow") {}
+    
+    
+        struct go_dark_iron_pillowAI : public GameObjectAI
+        {
+            go_dark_iron_pillowAI(GameObject* go) : GameObjectAI(go) {};
+    
+            bool GossipHello(Player* player) override
+            {
+   
+                if (!player->FindNearestGameObject(GAMEOBJECT_DARK_IRON_PILLOW_EVENT_GENERATOR, 200.0f))
+                {
+                    Position pos = { -6545.88f, -1345.29f, 208.89f };   
+                    player->SummonGameObject(GAMEOBJECT_DARK_IRON_PILLOW_EVENT_GENERATOR, pos, QuaternionData(), 0, GO_SUMMON_TIMED_DESPAWN);
+                }
+    
+                return false;
+            }
+        };
+    
+        GameObjectAI* GetAI(GameObject* go) const override
+        {
+            return new go_dark_iron_pillowAI(go);
+        }
+};
+
+class go_dark_iron_pillow_event_generator : public GameObjectScript
+{
+    public:
+        go_dark_iron_pillow_event_generator() : GameObjectScript("go_dark_iron_pillow_event_generator") {}
+    
+    
+        struct go_dark_iron_pillow_event_generatorAI : public GameObjectAI
+        {
+            go_dark_iron_pillow_event_generatorAI(GameObject* go) : GameObjectAI(go), owner(go) {};
+    
+            void InitializeAI() override
+            {
+                events.ScheduleEvent(EVENT_WAVE_1,       60 * IN_MILLISECONDS, 0, 0); 
+                events.ScheduleEvent(EVENT_WAVE_2,      180 * IN_MILLISECONDS, 0, 0); 
+                events.ScheduleEvent(EVENT_CHAMBERMAID, 300 * IN_MILLISECONDS, 0, 0); 
+            }
+
+            void UpdateAI(uint32 diff) override
+            {
+                events.Update(diff);
+
+                while (uint32 eventId = events.ExecuteEvent())
+                {
+                    switch (eventId)
+                    {
+                    case EVENT_WAVE_1:
+                        for (size_t i = 0; i < 10; i++)
+                        {
+                            if (TempSummon* summon = owner->SummonCreature(NPC_SLEEPY_DARK_IRON_WORKER, -6568.03f, -1306.73f, 208.70f, 0, TEMPSUMMON_TIMED_DESPAWN_OUT_OF_COMBAT, 120 * IN_MILLISECONDS))
+                            {
+                                summon->ToCreature()->GetMotionMaster()->MovePath(NPC_SLEEPY_DARK_IRON_WORKER * 100 + i, true);
+                                if (i == 0)
+                                    summon->ToCreature()->AI()->Talk(SAY_IRON_WORKER_0);
+                            }
+                        }
+                        break;
+                    case EVENT_WAVE_2:
+                        for (size_t i = 0; i < 10; i++)
+                        {
+                            if (TempSummon* summon = owner->SummonCreature(NPC_SLEEPY_DARK_IRON_WORKER, -6481.22f, -1349.06f, 212.10f, 0, TEMPSUMMON_TIMED_DESPAWN_OUT_OF_COMBAT, 120 * IN_MILLISECONDS))
+                            {
+                                summon->ToCreature()->GetMotionMaster()->MovePath(NPC_SLEEPY_DARK_IRON_WORKER * 100 + 10 + i, true);
+    
+                                if (i == 0)
+                                    summon->ToCreature()->AI()->Talk(SAY_IRON_WORKER_1);
+                            }
+                        }
+                        break;
+                    case EVENT_CHAMBERMAID:
+                        owner->SummonCreature(NPC_CHAMBERMAID_PILLACLENCHER, -6545.88f, -1345.29f, 208.89f, 0, TEMPSUMMON_TIMED_DESPAWN_OUT_OF_COMBAT, 300 * IN_MILLISECONDS);
+                        owner->Delete();
+                        break;
+                    }
+                }
+            }
+    
+            EventMap events;
+            GameObject *owner;
+        };
+    
+        GameObjectAI* GetAI(GameObject* go) const override
+        {
+            return new go_dark_iron_pillow_event_generatorAI(go);
+        }
+};
+
 void AddSC_go_scripts()
 {
     new go_gilded_brazier();
@@ -2011,4 +2120,6 @@ void AddSC_go_scripts()
     new go_darkmoon_faire_music();
     new go_pirate_day_music();
     new go_bells();
+    new go_dark_iron_pillow();
+    new go_dark_iron_pillow_event_generator();
 }
