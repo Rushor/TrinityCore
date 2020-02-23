c344068f82a272aee4f7c4a9a65b4c28b873dd6b
Rushor <PBienati@web.de>
2020-02-23 17:37:41 +0100

    Scripts/Spells: Add spellscriot for Magic Rooster

---
+++ sql/fixes/2020_02_23_01_world.sql
@@ -0,0 +1,4 @@
+-- spell_gen_magic_rooster
+DELETE FROM `spell_script_names` WHERE `ScriptName`='spell_gen_magic_rooster';
+INSERT INTO `spell_script_names` (`spell_id`,`ScriptName`) VALUES
+(65917, 'spell_gen_magic_rooster');
