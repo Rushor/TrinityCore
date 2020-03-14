6d3811a18cdf11a056050ccb1bbf39bbb43e33ce
Rushor <PBienati@web.de>
2020-03-14 23:46:06 +0100

    DB/Gameobject: Add Pygmy Suckerfish loot to all fish swarms of northrend.
    * source: Pygmy Suckerfish are primarily caught from the schools of fish found in Northrend but have a smaller chance to be found in open water. Schools of fish, with the exception of Crystalsong Forest have about a 65% chance to drop suckerfish, while open water has about a 10% chance. Note that the suckerfish is an adittional catch to the fish you catch from the school or body of water, you will never catch a pygmy suckerfish alone. (http://www.wowwiki.com/Pygmy_Suckerfish)

---
+++ sql/fixes/2020_03_14_01_world.sql
@@ -0,0 +1,35 @@
+-- 
+/* Glacial Salmon School*/
+DELETE FROM `gameobject_loot_template` WHERE `entry`=25662 AND `item`=40199;
+INSERT INTO `gameobject_loot_template` (`entry`, `item`, `Chance`, `lootmode`, `groupid`, `mincount`, `maxcount`) VALUES 
+(25662, 40199, 65, 1, 2, 1, 1);
+
+/* Moonglow Cuttlefish School */
+DELETE FROM `gameobject_loot_template` WHERE `entry`=25670 AND `item`=40199;
+INSERT INTO `gameobject_loot_template` (`entry`, `item`, `Chance`, `lootmode`, `groupid`, `mincount`, `maxcount`) VALUES 
+(25670, 40199, 65, 1, 2, 1, 1);
+
+/* Nettlefish School */
+DELETE FROM `gameobject_loot_template` WHERE `entry`=25673 AND `item`=40199;
+INSERT INTO `gameobject_loot_template` (`entry`, `item`, `Chance`, `lootmode`, `groupid`, `mincount`, `maxcount`) VALUES 
+(25673, 40199, 65, 1, 2, 1, 1);
+
+/* Borean Man O' War School */
+DELETE FROM `gameobject_loot_template` WHERE `entry`=25669 AND `item`=40199;
+INSERT INTO `gameobject_loot_template` (`entry`, `item`, `Chance`, `lootmode`, `groupid`, `mincount`, `maxcount`) VALUES 
+(25669, 40199, 65, 1, 2, 1, 1);
+
+/* Imperial Manta Ray School */
+DELETE FROM `gameobject_loot_template` WHERE `entry`=25668 AND `item`=40199;
+INSERT INTO `gameobject_loot_template` (`entry`, `item`, `Chance`, `lootmode`, `groupid`, `mincount`, `maxcount`) VALUES 
+(25668, 40199, 65, 1, 2, 1, 1);
+
+/* Dragonfin Angelfish School */
+DELETE FROM `gameobject_loot_template` WHERE `entry`=25664 AND `item`=40199;
+INSERT INTO `gameobject_loot_template` (`entry`, `item`, `Chance`, `lootmode`, `groupid`, `mincount`, `maxcount`) VALUES 
+(25664, 40199, 65, 1, 2, 1, 1);
+
+/* Fangtooth Herring School */
+DELETE FROM `gameobject_loot_template` WHERE `entry`=25663 AND `item`=40199;
+INSERT INTO `gameobject_loot_template` (`entry`, `item`, `Chance`, `lootmode`, `groupid`, `mincount`, `maxcount`) VALUES 
+(25663, 40199, 65, 1, 2, 1, 1);
