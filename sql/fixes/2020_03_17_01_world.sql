d6a0c89581f636dce5ea11c7f841f7b2efd56238
Rushor <PBienati@web.de>
2020-03-17 19:00:19 +0100

    DB/Instance: AQ40 - Add missing spawn for Ouro.

---
+++ sql/fixes/2020_03_17_01_world.sql
@@ -0,0 +1,5 @@
+-- 
+DELETE FROM `creature` WHERE `id` = 15517;
+DELETE FROM `creature` WHERE `guid` = 188073;
+INSERT INTO `creature` (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `wander_distance`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`) VALUES 
+(188073, 15517, 531, 1, 1, 0, 0, -9188.45, 2091.56, -64.17, 6.01, 6380, 0, 0, 4548000, 0, 0, 0, 0, 0);
